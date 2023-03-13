95c51ba72860a0879d812a6d178b05d8d2563812
Jude Boachie <judeboachiekwame@gmail.com>
2023-03-13 13:52:14 +0000

    Pending changes exported from your codespace

--- main.py
+++ main.py
@@ -7,18 +7,21 @@ import openai
 from printer import type_it
 
 # Set up the OpenAI API client
-with open("your_api_key.txt", 'r') as file:
+with open("my_key.txt", 'r') as file:
     openai.api_key = file.read()
 
 # Choose a GPT-2 model size
-model_engine = "text-davinci-002"
+model_engine = "gpt-3.5-turbo"
 
 # Generate text using GPT-2
 prompt_text = input(" How may I help you? (Type 'stop' to exit.)\n$ ")
 while True:
     if prompt_text.lower() == "stop":
         break
-    response = openai.Completion.create(engine=model_engine, prompt=prompt_text, max_tokens=100)
+    response = openai.ChatCompletion.create(engine=model_engine,
+                                        prompt=prompt_text,
+                                        max_tokens=100,
+                                        temperature=0.7,)
     generated_text = response.choices[0].text
     type_it(generated_text)
     prompt_text = input("\n$ ")
